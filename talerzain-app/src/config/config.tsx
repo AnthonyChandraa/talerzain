ba59e1a18efc6e940f455864dbd6a49a9bceae29
ElodiaNadine <116774812+ElodiaNadine@users.noreply.github.com>
2022-11-06 19:17:01 +0700

    ohh ini ya

--- talerzain-app/src/config/config.tsx
+++ talerzain-app/src/config/config.tsx
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyD_tMYPgG9-tOz9RTNqONykZdlX1h8OzoM",
   authDomain: "talerzain-aw.firebaseapp.com",
@@ -16,6 +14,7 @@ const firebaseConfig = {
   measurementId: "G-76BZNP84GW"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
--- talerzain-app/src/contexts/UserProvider.tsx
+++ talerzain-app/src/contexts/UserProvider.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useState, useContext, createContext, useEffect} from 'react';
+import {auth} from './../config/config';
+import {onAuthStateChanged} from 'firebase/auth';
+import firebase from "firebase/compat/app";
 
-function UserProvider() {
-  return (
-    <div>UserProvider</div>
-  )
+
+interface IUserContext{
+  user: firebase.User
 }
 
-export default UserProvider
+type Props = {
+  children?: React.ReactNode;
+}
+
+const UserContext = createContext<firebase.User | null>(null);
+function UserProvider({children}:Props) {
+  const [currentUser, setCurrentUser] = useState<firebase.User|null>(null);
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (firebaseUser)=>{
+       if(firebaseUser!=null){
+        // setCurrentUser(firebaseUser);
+       }
+    })
+  }, []);
+
+
+  return <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>
+}
+
+export default UserProvider;
