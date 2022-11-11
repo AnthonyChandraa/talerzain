cc7f2628c86065d017e991354cb53dcbffe18336
ElodiaNadine <116774812+ElodiaNadine@users.noreply.github.com>
2022-11-11 19:34:29 +0700

    Sign-In modal hook yey

--- talerzain-app/src/components/navbar/Navbar.tsx
+++ talerzain-app/src/components/navbar/Navbar.tsx
@@ -1,16 +1,45 @@
 import React from 'react'
+import { useModal } from 'react-hooks-use-modal';
+import Register from '../../pages/auth/Register';
+import SignIn from '../../pages/auth/SignIn';
 
 function Navbar() {
+  const [ModalSignIn, openIn, closeIn, isOpenIn] = useModal('root', {
+    preventScroll: true,
+    focusTrapOptions: 
+    {
+      clickOutsideDeactivates: true,
+    },
+  });
+
+  const [ModalSignUp, openUp, closeUp, isOpenUp] = useModal('root', {
+    preventScroll: true,
+    focusTrapOptions: 
+    {
+      clickOutsideDeactivates: true,
+    },
+  });
+
   return (
-    <div className='d-flex justify-content-between'>
-      <div className='m-4 h2 '>
+    <>
+    <div className='d-flex justify-content-between align-items-center'>
+      <div className='m-4 h2'>
         <p>talerzain.</p>
       </div>
       <div className='nav-btn-sign'>
-        <a className='btn-sign bg-white shadow-btn' href="">Sign In</a>
-        <a className='btn-sign bg-yellow shadow-btn' href="">Sign Up</a>
+        <button onClick={openIn}className='btn-sign bg-white shadow-btn' >Sign In</button>
+        <button onClick={openUp} className='btn-sign bg-yellow shadow-btn' >Sign Up</button>
       </div>
     </div>
+
+    <ModalSignIn>
+      <SignIn />
+    </ModalSignIn>
+
+    <ModalSignUp>
+      <Register />
+    </ModalSignUp>
+    </>
   )
 }
 
---
+++ talerzain-app/src/pages/auth/SignIn.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import fotoSignIn from '../../assets/Assets [Talerzain]/Assets [Talerzain]/Signup Signin/Artist.gif'
+import facebook from '../../assets/Assets [Talerzain]/Assets [Talerzain]/Signup Signin/icons8-facebook-144.png'
+import google from '../../assets/Assets [Talerzain]/Assets [Talerzain]/Signup Signin/icons8-google-128.png'
+import twitter from '../../assets/Assets [Talerzain]/Assets [Talerzain]/Signup Signin/icons8-twitter-240.png'
+
+
+function SignIn() {
+  return (
+    <div className='sign-in'>
+      <div className='d-flex justify-content-center align-items-center ps-4 pe-4' style={{width: "282px", height:"282px"}}>
+        <img src={fotoSignIn} alt="no photo" className='photo rounded-5' style={{width: "280px"}}/>
+      </div>
+      <div className='bg-yellow-op-3 sign-in-right'>
+          <div className='d-flex justify-content-start w-75'>
+            <p className='m-4 h4'>Sign In</p>
+          </div>
+          <div className='d-flex flex-column mb-3'>
+            <label className='mini-links' htmlFor="form-username">Username</label>
+            <input className='form-input shadow-btn ' type="text" id='form-username' placeholder='insert username'/>
+          </div>
+          <div className='d-flex flex-column'>
+            <label className='mini-links' htmlFor="form-password">Password</label>
+            <input className='form-input shadow-btn' type="password" id='form-password' placeholder='insert password'/>
+          </div>
+          <div className='d-flex flex-column align-items-center'>
+            <button className='rounded-4 btn-sign bg-red shadow-btn in-button'>Log In</button>
+            <a className='mini-links m-2' href="">Need an account?</a>
+          </div>
+          <div className='align-items-center d-flex flex-column' style={{gap: "10px"}}>
+            <p className='mini-links'> ─── &nbsp; or sign in with &nbsp; ─── </p>
+            <div className='d-flex align-items-center flex-row' style={{gap: "20px"}}>
+            <img src={facebook} alt="no photo" className='logo-img'/>
+            <img src={google} alt="no photo" className='logo-img'/>
+            <img src={twitter} alt="no photo" className='logo-img'/>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
