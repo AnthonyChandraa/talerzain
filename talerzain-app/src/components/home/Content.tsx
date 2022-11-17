75a6e3a36afcc87e63bba589dc050e528fd5aadf
ElodiaNadine <116774812+ElodiaNadine@users.noreply.github.com>
2022-11-17 14:00:13 +0700

    nambahin page submenu yey

--- talerzain-app/src/components/home/Content.tsx
+++ talerzain-app/src/components/home/Content.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import artCollectibles from '../../assets/Assets [Talerzain]/Assets [Talerzain]/Homepage/icons8-art-64.png'
 import creativeServices from '../../assets/Assets [Talerzain]/Assets [Talerzain]/Homepage/icons8-creative-64.png'
 import handcraft from '../../assets/Assets [Talerzain]/Assets [Talerzain]/Homepage/icons8-wool-64.png'
+import { Link } from 'react-router-dom'
 
 
 function Content() {
   return (
     <div className='grid-home'>
-        <a href="" className='navigation1'>
+        <Link to="src\pages\submenus\artcollection.tsx" className='navigation1'>
           <img src={artCollectibles} alt="no photo" width={80}/>
           <p className='m-2'>Art & Collectibles</p>
-        </a>
+        </Link>
         <a href="" className='navigation1'>
           <img src={handcraft} alt="no photo" width={80}/>
           <p className='m-2'>Handcrafts</p>
---
+++ talerzain-app/src/pages/submenus/artcollection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function artcollection() {
+  return (
+
+    <div>art-collection</div>
+   
+  )
+}
+
+export default artcollection
